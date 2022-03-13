e0f38a0c8f2cb03d942bfccaf8082a1a0d5659e7
Rich Burdon <rich.burdon@gmail.com>
2022-03-12 21:30:36 -0500

    Config.

--- scripts/setup-git.sh
+++ scripts/setup-git.sh
@@ -4,7 +4,11 @@ cp -f ./config/.gitconfig ~/
 
 git config --global pager.branch false
 
+# https://cli.github.com
 gh config set pager cat
 
 # https://www.npmjs.com/package/git-branch-select
 npm install --global git-branch-select
+
+# https://github.com/nosarthur/gita
+pip3 install -U gita
